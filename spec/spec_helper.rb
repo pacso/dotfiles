aa9de4295459cd5127b777b50443429ef603a60c
Jon Pascoe <jon.pascoe@me.com>
2014-05-26 17:32:20 +0100

    Setup fixture data in tmp dir before each test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,24 @@
 require 'rubygems'
 require 'rake'
 
-SOURCE_BASE_PATH      = File.join(File.dirname(__FILE__), 'fixture', 'sources')
-TARGET_BASE_PATH = File.join(File.dirname(__FILE__), 'fixture', 'target')
+FIXTURE_SOURCE_PATH = File.join(File.dirname(__FILE__), 'fixture', 'sources')
+FIXTURE_TARGET_PATH = File.join(File.dirname(__FILE__), 'fixture', 'target')
+SPEC_TMP_DIR        = File.join(File.dirname(__FILE__), 'tmp')
+SOURCE_BASE_PATH    = File.join(File.dirname(__FILE__), 'tmp', 'sources')
+TARGET_BASE_PATH    = File.join(File.dirname(__FILE__), 'tmp', 'target')
 
 require File.join(File.dirname(File.dirname(__FILE__)), 'lib', 'config', 'init')
 
 RSpec.configure do |config|
   config.order = 'random'
+
+  config.before(:each) do
+    Dir.mkdir SPEC_TMP_DIR
+    FileUtils.cp_r(FIXTURE_SOURCE_PATH, SOURCE_BASE_PATH)
+    FileUtils.cp_r(FIXTURE_TARGET_PATH, TARGET_BASE_PATH)
+  end
+
+  config.after(:each) do
+    FileUtils.rm_r SPEC_TMP_DIR
+  end
 end
